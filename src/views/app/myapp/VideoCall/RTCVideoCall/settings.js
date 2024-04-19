53d367aca40cdcd4c22729b0fa06ce83055e4b43
brovinRoy <brovinroy@gmail.com>
2024-04-19 22:02:11 +0530

    token

--- src/views/app/myapp/VideoCall/RTCVideoCall/settings.js
+++ src/views/app/myapp/VideoCall/RTCVideoCall/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "4e6374a1eddd4d20a604fb0513fae8fb";
 const token =
-  "007eJxTYODmapt053l24MIFJjaTFv7LXqr8/59cY9kUWUEjLkXHtUUKDCapZsbmJomGqSkpKSYpRgaJZgYmaUkGpobGaYmpFmlJ7/UU0hoCGRlSOc2ZGBkgEMRnYUjPT89nYAAA6Bkddw==";
+  "007eJxTYLg2nUt3auaBxZsv74mtipHdK2W6+nLY4pCiFxMdVmhJ5a9QYDBJNTM2N0k0TE1JSTFJMTJINDMwSUsyMDU0TktMtUhLEpZUSmsIZGQwOP2SiZEBAkF8Fob0/PR8BgYASlwfEQ==";
 
 export const config = { mode: "rtc", codec: "vp8", appId, token };
 export const useClient = createClient(config);
