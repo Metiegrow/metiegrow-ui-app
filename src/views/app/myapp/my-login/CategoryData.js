da81e8bf73c256120aec411c6ecb4823fe2cdc03
brovinRoy <brovinroy@gmail.com>
2024-06-13 17:15:41 +0530

    Category

---
+++ src/views/app/myapp/my-login/CategoryData.js
@@ -0,0 +1,20 @@
+const CategoryData = [
+    {
+        "name" : "Engineering & Data",
+        "short": "ED"
+    },
+    {
+        "name" : "UX & Design",
+        "short" : "UD"
+    },
+    {
+        "name" : "Business & Management",
+        "short" : "BM"
+    },
+    {
+        "name" : "Product & Marketing",
+        "short" : "PM"
+    }
+  ]
+
+  export default CategoryData;
